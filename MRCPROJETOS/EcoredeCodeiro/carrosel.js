3aa2c67d10b4e4ccf41e87773bae31bfcba75596
Rafael Santos <126109568+Rafaeelsantos@users.noreply.github.com>
2025-07-02 15:05:35 -0300

    Delete MRCPROJETOS/EcoredeCodeiro directory

--- MRCPROJETOS/EcoredeCodeiro/carrosel.js
+++
@@ -1,8 +0,0 @@
-const imagens = document.querySelectorAll('.carrossel-img');
-let index = 0;
-
-setInterval(() => {
-    imagens[index].classList.remove('active');
-    index = (index + 1) % imagens.length;
-    imagens[index].classList.add('active');
-}, 5000); // troca a cada 5 segundos
